a45f8b7a5e2b9f6f50cd83b3b3eee29698a3345e
irgidp <irgijak@gmail.com>
2021-08-04 17:39:11 +0700

    First Commit TUBES Provis

---
+++ src/kemahasiswaan_10119059_10119084/Kemahasiswaan_10119059_10119084.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kemahasiswaan_10119059_10119084;
+
+/**
+ *
+ * @author Irgi
+ */
+public class Kemahasiswaan_10119059_10119084 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frm_mhs mhs = new frm_mhs();
+        mhs.setVisible(true);
+    }
+    
+}
